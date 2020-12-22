574a9f9770d41b4c40eafd2fbda93f4266d27d45
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-12-22 21:45:58 +0100

    Transform to_midi and from_midi to use seconds for time

--- midi.py
+++ midi.py
@@ -1,57 +1,69 @@
-from mido import MidiFile, MidiTrack, tick2second, second2tick
+from mido import MidiFile, MidiTrack, Message, second2tick, tick2second
 from collections import namedtuple
 import numpy as np
-import math
 import cv2
 
-Note = namedtuple('Note', 'value time velocity')
+
+def to_midi(arr, midi_path):
+    n_tracks = (arr != 0).sum(axis=0).max()
+    bpm = 96
+    ticks_per_seconds = second2tick(1, bpm, 500000)
+
+    mid = MidiFile()
+    tracks = list(map(lambda x: MidiTrack(), range(n_tracks * 2)))
+    prev_column = np.zeros(128, np.int8)
+    recent_velocity_changes = np.zeros(len(tracks), np.int8)
+    for t, column in enumerate(arr.T):
+        column_track_index = 0
+        difference = column - prev_column
+        for note, velocity_change in enumerate(difference):
+            if velocity_change == 0:
+                continue
+            time_since_last_change = t - recent_velocity_changes[column_track_index]
+            time = round(time_since_last_change / 16 * ticks_per_seconds)
+            print(time_since_last_change)
+
+            if column[note] == 0:
+                message = Message('note_off', note=note, velocity=0, time=time)
+            else:
+                message = Message('note_on', note=note, velocity=column[note], time=time)
+
+            recent_velocity_changes[column_track_index] = t
+            tracks[column_track_index].append(message)
+            column_track_index += 1
+        prev_column = column
+
+    for track in tracks:
+        mid.tracks.append(track)
+    mid.save(midi_path)
 
 
-def track_to_notes(track):
+def from_midi(midi_path, img_output='output/arr.png'):
+    Note = namedtuple('Note', 'value time velocity')
+    a = MidiFile(midi_path)
     time = 0
     notes = []
-    for msg in track:
+    max_t = 0
+    for msg in a:
         time += msg.time
         if msg.type == 'note_on' or msg.type == 'note_off':
-            notes.append(Note(msg.note, time, msg.velocity if msg.type == 'note_on' else 0))
-    return notes
+            t = int(time * 16)
+            max_t = t if t > max_t else max_t
+            v = msg.velocity if msg.type == 'note_on' else 0
+            notes.append(Note(msg.note, t, v))
 
+    arr = np.zeros((128, max_t), np.int8)
+    for note in notes:
+        arr[note.value, note.time:] = note.velocity
 
-def notes_to_array(tick_duration):
-    def x(notes):
-        arr = np.zeros((128, tick_duration), np.int8)
-        for note in notes:
-            arr[note.value, note.time:] = note.velocity
-
-        return arr
-
-    return x
-
-
-def from_midi(midi_path):
-    a = MidiFile(midi_path)
-    tempo = get_tempo(a)
-    tick_duration = math.ceil(second2tick(a.length, a.ticks_per_beat, tempo))
-    notes_data = list(filter(lambda x: x, map(track_to_notes, a.tracks)))
-    array_data = list(map(notes_to_array(tick_duration), notes_data))
-
-    for i, arr in enumerate(array_data):
-        cv2.imwrite(f"output/img{i}.png", arr)
-
-    print("done")
-
-
-def get_tempo(midi_file):
-    for track in midi_file.tracks:
-        for note in track:
-            if note.type == "set_tempo":
-                return note.tempo
-    return 50000
+    cv2.imwrite(img_output, arr)
+    # print(arr)
+    return arr
 
 
 if __name__ == '__main__':
-    from_midi("output/unfin.midi")
-    # from_midi("output/zeppelin.mid")
-    # a = np.zeros((10, 10), np.int8)
-    # a[1, 5:] = 30
-    # print(a)
+    arr = from_midi("output/unfin.midi")
+    to_midi(arr, 'output/unfin_result.midi')
+
+    # from_midi("output/unfin_result.midi", 'output/unfin_result.png')
+    # from_midi("output/unfin.midi", 'output/unfin.png')
